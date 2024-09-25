e1e6b07f6c770e580f34f33892e47a44d96bebbe
Chezka Siño <chezka@pm.me>
2024-09-24 20:54:50 -0700

    purchases update

--- src/main.py
+++ src/main.py
@@ -45,6 +45,28 @@ def add_to_customers(supabase):
     
     data = supabase.table('customers').insert(cust_json).execute()
 
+def classes():
+    file = input('File path: ')
+    data = pd.read_csv(file)
+
+    # renaming columns to match table
+    data = data.rename(columns={
+        'Class': 'class_name',
+        'Date': 'day',
+        'Time': 'class_start'
+    })
+
+    # keeping needed columns
+    class_info = data[['class_name', 'day', 'class_start']]
+
+    return class_info
+
+def add_class(supabase):
+    class_list = classes()
+    class_json = loads(class_list.to_json(orient='records'))
+    
+    data = supabase.table('classes').insert(class_json).execute()
+
 def add_pass(supabase):
     
     file = input('File path: ')
@@ -52,6 +74,38 @@ def add_pass(supabase):
     pass_json = loads(pass_list.to_json(orient='records'))
     
     data = supabase.table('passes').insert(pass_json).execute()
+
+def purchases():
+    file = input('File path: ')
+    data = pd.read_csv(file)
+
+    data = data.rename(columns={
+        'Customer ID': 'user_id',
+        'Pass': 'pass_name',
+        'Purchased': 'purchase_date',
+        'Paid with': 'method'
+    })
+
+    purchase_info = data[['user_id', 'pass_name', 'purchase_date', 'method']]
+
+    return purchase_info
+
+def add_purchases(supabase):
+
+    purchase_list = purchases()
+    
+    # pulling pass data to merge
+    passes = pd.DataFrame.from_records(
+        supabase.table('passes').select('*').execute().data)
+    
+    merged_data = purchase_list.merge(passes, left_on='pass_name', right_on='pass_name',
+                                      how='left')
+    merged_data = merged_data.drop(['pass_name','punches','price'], axis=1)
+    merged_data = merged_data.rename(columns={'id':'pass_id'})
+
+    purchase_json = loads(merged_data.to_json(orient='records'))
+    
+    data = supabase.table('purchases').insert(purchase_json).execute()
 
 if __name__ == "__main__":
     
@@ -63,6 +117,8 @@ if __name__ == "__main__":
     print("Menu")
     print("1. Add customer data")
     print("2. Add pass")
+    print("3. Add classes")
+    print("4. Add purchases")
 
     task = int(input('Select: ' ))
 
@@ -70,3 +126,7 @@ if __name__ == "__main__":
         add_to_customers(supabase)
     elif task == 2:
         add_pass(supabase)
+    elif task == 3:
+        add_class(supabase)
+    elif task == 4:
+        add_purchases(supabase)
